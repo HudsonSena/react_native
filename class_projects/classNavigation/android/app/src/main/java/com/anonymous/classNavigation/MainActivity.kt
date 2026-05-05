1c90cae877830647ec39e2a3cebed98993a618a7
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-05-05 13:35:17 -0300

    update for new project

--- class_projects/classNavigation/android/app/src/main/java/com/anonymous/classNavigation/MainActivity.kt
+++ class_projects/classNavigation/android/app/src/main/java/com/anonymous/classNavigation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.classnavigation
+package com.anonymous.classNavigation
 
 import android.os.Build
 import android.os.Bundle
--- class_projects/classNavigation/android/app/src/main/java/com/anonymous/classNavigation/MainApplication.kt
+++ class_projects/classNavigation/android/app/src/main/java/com/anonymous/classNavigation/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.classnavigation
+package com.anonymous.classNavigation
 
 import android.app.Application
 import android.content.res.Configuration
