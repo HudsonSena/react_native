60196d4f588abfecfdb1f27472a5b651ca1ab702
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-05-06 11:17:24 -0300

    Update App.tsx

--- class_projects/classNavigation/App.tsx
+++ class_projects/classNavigation/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Agora foi</Text>
+      <Text>Agora foi Oficial</Text>
       <StatusBar style="auto" />
     </View>
   );
