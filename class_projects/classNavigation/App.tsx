7f252eb9305d292fab48f8f54fc82a43126e42b0
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-05-06 10:49:12 -0300

    Update App.tsx

--- class_projects/classNavigation/App.tsx
+++ class_projects/classNavigation/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hudson Diego</Text>
+      <Text>Agora foi</Text>
       <StatusBar style="auto" />
     </View>
   );
